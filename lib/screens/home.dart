c1af8b1d2bf5c78a27e52686ccb501f390057d0b
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-01-29 21:56:58 -0800

    redid routing, styling for game screen.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
                 RaisedButton( 
                   child: Text('Play Quick Game'),
                   onPressed: () {
-                    Navigator.pushReplacementNamed(context, QuickGameScreen.routeName);
+                    Navigator.pushNamed(context, QuickGameScreen.routeName);
                   },
                 ),
                 FlatButton( 
